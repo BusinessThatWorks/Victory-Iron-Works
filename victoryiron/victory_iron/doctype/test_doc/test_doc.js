bd447627bc586fa01b7c5d5b940a476764933979
beetashoke <beetashoke.chakraborty@clapgrow.com>
2025-05-21 10:10:25 +0530

    Initial commit

---
+++ victoryiron/victory_iron/doctype/test_doc/test_doc.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2025, beetashokechakraborty and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Test doc", {
+    refresh(frm) {
+        console.log("first")
+
+    },
+});

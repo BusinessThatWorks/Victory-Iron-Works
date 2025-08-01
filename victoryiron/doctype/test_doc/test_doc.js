0d7e17cd70b0a45103204612e207c2fbac7a816b
beetashoke <beetashoke.chakraborty@clapgrow.com>
2025-08-01 11:49:41 +0530

    init

--- victoryiron/doctype/test_doc/test_doc.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2025, beetashokechakraborty and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on("Test doc", {
-    refresh(frm) {
-        console.log("see console");
-
-    },
-});

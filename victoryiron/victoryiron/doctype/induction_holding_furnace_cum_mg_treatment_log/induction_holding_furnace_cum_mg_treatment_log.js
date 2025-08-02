5cdee9c9203f1c98e24a1470675756106f30fee2
JaiDeb <erp@clapgrow.com>
2025-08-02 15:01:59 +0530

    cupola2

---
+++ victoryiron/victoryiron/doctype/induction_holding_furnace_cum_mg_treatment_log/induction_holding_furnace_cum_mg_treatment_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, beetashokechakraborty and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Induction Holding Furnace Cum Mg Treatment Log", {
+// 	refresh(frm) {
+
+// 	},
+// });

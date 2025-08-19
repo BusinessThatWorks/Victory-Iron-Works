59545b71d8d9a437982eafd8c8c4e49457a5c4c2
JaiDeb <erp@clapgrow.com>
2025-08-19 11:34:03 +0530

    Heat Process

---
+++ victoryiron/victoryiron/doctype/chemical_analysis_of_cast_metal/chemical_analysis_of_cast_metal.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, beetashokechakraborty and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Chemical Analysis Of Cast Metal", {
+// 	refresh(frm) {
+
+// 	},
+// });

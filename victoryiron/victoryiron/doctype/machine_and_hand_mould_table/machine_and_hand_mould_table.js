4706cc08b7330adeb9bc718820d716634584440a
JaiDeb <erp@clapgrow.com>
2025-08-28 12:34:05 +0530

    update

---
+++ victoryiron/victoryiron/doctype/machine_and_hand_mould_table/machine_and_hand_mould_table.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, beetashokechakraborty and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Machine And Hand Mould Table", {
+// 	refresh(frm) {
+
+// 	},
+// });

5f4c9a36373928ed1c1c59cfda1bb310c6e948d0
beetashoke <beetashoke.chakraborty@clapgrow.com>
2025-08-29 17:36:38 +0530

    induction log

---
+++ victoryiron/victoryiron/doctype/attendence/test_attendence.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, beetashokechakraborty and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAttendence(FrappeTestCase):
+	pass

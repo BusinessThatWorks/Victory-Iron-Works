e7024b85c0e0d3e8567c90a1d0c20196cf719357
JaiDeb <erp@clapgrow.com>
2025-09-01 11:07:45 +0530

    Pouring sheet

---
+++ victoryiron/victoryiron/doctype/pouring_traceability_table/pouring_traceability_table.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, beetashokechakraborty and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Pouring Traceability Table", {
+// 	refresh(frm) {
+
+// 	},
+// });

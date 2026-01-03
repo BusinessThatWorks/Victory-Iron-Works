7fcde7b8af36050a6ecd30ea1565d7fa4891adc8
Md Sarim Rashid Khan <sarim.khan@clapgrow.com>
2026-01-03 13:43:55 +0530

    cleaned js file for optimization

--- victoryiron/victoryiron/page/chemical_analysis_of/chemical_analysis_of.js
+++ victoryiron/victoryiron/page/chemical_analysis_of/chemical_analysis_of.js
@@ -18,17 +18,20 @@ frappe.pages['chemical-analysis-of'].on_page_load = function(wrapper) {
 	load_data();
 }
 
-function load_data(){
-    let from = $("#from_date").val();
-    let to = $("#to_date").val();
+function load_data() {
+	let from = $("#from_date").val();
+	let to = $("#to_date").val();
 
-    frappe.call({
-        method:"victoryiron.api.chemical_analysis.get_furnace_bath",  // <- next step
-        args:{ from_date: from, to_date: to },
-        callback:function(r){
-            render_table(r.message || []);
-        }
-    });
+	frappe.call({
+		method: "victoryiron.api.chemical_analysis.get_furnace_bath", // <- next step
+		args: {
+			from_date: from || null,
+			to_date: to || null,
+		},
+		callback: function (r) {
+			render_table(r.message || []);
+		},
+	});
 }
 function formatTimeToAMPM(time) {
     if (!time) return "";
@@ -41,6 +44,16 @@ function formatTimeToAMPM(time) {
 function render_table(data){
     let tbody = $("#result_table tbody");
     tbody.empty();
+    if (!data.length) {
+    tbody.append(`
+        <tr>
+            <td colspan="15" class="text-center text-muted">
+                No records found
+            </td>
+        </tr>
+    `);
+    return;
+}
 
     data.forEach(row => {
         tbody.append(`
@@ -64,3 +77,11 @@ function render_table(data){
         `);
     });
 }
+$("#export_btn").on("click", function () {
+    let from = $("#from_date").val();
+    let to = $("#to_date").val();
+
+    let url = `/api/method/victoryiron.api.chemical_analysis.export_furnace_bath_excel?from_date=${from || ""}&to_date=${to || ""}`;
+    window.open(url);
+});
+

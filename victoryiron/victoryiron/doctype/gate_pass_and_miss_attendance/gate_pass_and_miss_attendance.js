3e335573503c0e587cd3b2c8cde3d8a7c4570a1c
beetashoke <beetashoke.chakraborty@clapgrow.com>
2026-02-20 13:06:50 +0530

    Custom field

---
+++ victoryiron/victoryiron/doctype/gate_pass_and_miss_attendance/gate_pass_and_miss_attendance.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, beetashokechakraborty and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Gate Pass and Miss Attendance", {
+// 	refresh(frm) {
+
+// 	},
+// });

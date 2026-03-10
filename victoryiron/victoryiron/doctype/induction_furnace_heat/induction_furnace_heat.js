8e0ab3a29cfdb884e94d6b472a29580346b0570a
Md Sarim Rashid Khan <sarim.khan@clapgrow.com>
2026-03-10 17:00:50 +0530

    induction furnace heat all calculations done

--- victoryiron/victoryiron/doctype/induction_furnace_heat/induction_furnace_heat.js
+++ victoryiron/victoryiron/doctype/induction_furnace_heat/induction_furnace_heat.js
@@ -1,8 +1,231 @@
-// Copyright (c) 2026, beetashokechakraborty and contributors
+// Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Induction Furnace Heat", {
-// 	refresh(frm) {
+frappe.ui.form.on("Induction Furnace Heat", {
+    refresh(frm) {
+        frm.trigger("firingprep_details");
+        frm.trigger("material_grade");
+    },
 
-// 	},
-// });
+        onload(frm) {
+        frm.trigger("firingprep_details");
+        frm.trigger("material_grade");
+        
+        // Add default items only for new documents
+        if (frm.is_new() && (!frm.doc.charge_mix_component_item || frm.doc.charge_mix_component_item.length === 0)) {
+            frappe.call({
+                method: "victoryiron.victoryiron.doctype.induction_furnace_heat.induction_furnace_heat.get_default_charge_mix_items",
+                callback: function(r) {
+                    if (r.message) {
+                        r.message.forEach(function(item_data) {
+                            frm.add_child("charge_mix_component_item", {
+                                item: item_data.item,
+                                weight_in_kg: 0,
+                                rate: item_data.rate,
+                                amount: 0
+                            });
+                        });
+                        frm.refresh_field("charge_mix_component_item");
+                    }
+                }
+            });
+        }
+    },
+
+    // 1. Checkbox - Hide all tabs except Firing/Prep and Details
+    firingprep_details(frm) {
+        let hide = frm.doc.firingprep_details ? 1 : 0;
+        
+        frm.set_df_property("charge_mix_tab", "hidden", hide);
+        frm.set_df_property("additional_cost_tab", "hidden", hide);
+        frm.set_df_property("extra_tab", "hidden", hide);
+        
+        frm.refresh_fields();
+    },
+
+    // 4. Material Grade - CI hides treatment, DI shows both, Default = CI behavior
+    material_grade(frm) {
+        let hide_treatment = 1; // Default: hide treatment (CI behavior)
+        
+        if (frm.doc.material_grade === "DI") {
+            hide_treatment = 0; // Show treatment for DI
+        }
+        
+        frm.set_df_property("treatment_consumption_section", "hidden", hide_treatment);
+        frm.set_df_property("table_bych", "hidden", hide_treatment);
+        frm.set_df_property("section_break_ovpy", "hidden", hide_treatment);
+        frm.set_df_property("total_treatment_quantity", "hidden", hide_treatment);
+        frm.set_df_property("total_treatment_valuation", "hidden", hide_treatment);
+        frm.set_df_property("column_break_ztnm", "hidden", hide_treatment);
+        
+        frm.refresh_fields();
+    },
+
+    // 6. Furnace Running Time Calculation
+    furnace_on_time(frm) {
+        calculate_furnace_running_time(frm);
+    },
+
+    furnace_off_time(frm) {
+        calculate_furnace_running_time(frm);
+    },
+
+    // 7. Furnace Unit Consumed Calculation
+    furnace_meter_reading_start(frm) {
+        calculate_furnace_unit_consumed(frm);
+    },
+
+    furnace_meter_reading_end(frm) {
+        calculate_furnace_unit_consumed(frm);
+    }
+});
+
+// 6. Calculate Furnace Running Time (in hours)
+function calculate_furnace_running_time(frm) {
+    if (frm.doc.furnace_on_time && frm.doc.furnace_off_time) {
+        let on_time = moment(frm.doc.furnace_on_time, "HH:mm:ss");
+        let off_time = moment(frm.doc.furnace_off_time, "HH:mm:ss");
+        
+        let diff_hours = off_time.diff(on_time, 'hours', true);
+        
+        // Handle next day case (night shift)
+        if (diff_hours < 0) {
+            diff_hours += 24;
+        }
+        
+        frm.set_value("total_furnace_running_time", diff_hours.toFixed(2) + " Hours");
+    }
+}
+
+// 7. Calculate Furnace Unit Consumed
+function calculate_furnace_unit_consumed(frm) {
+    let start = frm.doc.furnace_meter_reading_start;
+    let end = frm.doc.furnace_meter_reading_end;
+    
+    if (start !== null && start !== undefined && end !== null && end !== undefined) {
+        let consumed = flt(end) - flt(start);
+        frm.set_value("total_furnace_unit_consumed", consumed);
+    }
+}
+
+// 5. Calculate Charge Mix Totals
+function calculate_charge_mix_totals(frm) {
+    let total_weight = 0;
+    let total_valuation = 0;
+
+    (frm.doc.charge_mix_component_item || []).forEach(row => {
+        total_weight += flt(row.weight_in_kg);
+        total_valuation += flt(row.amount);
+    });
+
+    frm.set_value("total_charge_mix_kg", total_weight);
+    frm.set_value("total_charge_mix_valuation", total_valuation);
+}
+
+// 5. Calculate Treatment Totals
+function calculate_treatment_totals(frm) {
+    let total_quantity = 0;
+    let total_valuation = 0;
+
+    (frm.doc.table_bych || []).forEach(row => {
+        total_quantity += flt(row.weight_in_kg);
+        total_valuation += flt(row.amount);
+    });
+
+    frm.set_value("total_treatment_quantity", total_quantity);
+    frm.set_value("total_treatment_valuation", total_valuation);
+}
+
+
+// ============ CHARGE MIX COMPONENT CHILD TABLE ============
+frappe.ui.form.on("Charge Mix Component", {
+    
+    item(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (row.item) {
+            frappe.call({
+                method: "victoryiron.victoryiron.doctype.induction_furnace_heat.induction_furnace_heat.get_item_valuation_rate",
+                args: { item_code: row.item },
+                callback: function(r) {
+                    if (r.message !== undefined) {
+                        frappe.model.set_value(cdt, cdn, "rate", r.message);
+                        if (row.weight_in_kg) {
+                            let amount = flt(row.weight_in_kg) * flt(r.message);
+                            frappe.model.set_value(cdt, cdn, "amount", amount);
+                        }
+                        calculate_charge_mix_totals(frm);
+                    }
+                }
+            });
+        }
+    },
+
+    weight_in_kg(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (row.rate) {
+            let amount = flt(row.weight_in_kg) * flt(row.rate);
+            frappe.model.set_value(cdt, cdn, "amount", amount);
+        }
+        calculate_charge_mix_totals(frm);
+    },
+
+    rate(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (row.weight_in_kg) {
+            let amount = flt(row.weight_in_kg) * flt(row.rate);
+            frappe.model.set_value(cdt, cdn, "amount", amount);
+        }
+        calculate_charge_mix_totals(frm);
+    },
+
+    charge_mix_component_item_remove(frm) {
+        calculate_charge_mix_totals(frm);
+    }
+});
+
+
+// ============ TREATMENT MIX COMPONENT CHILD TABLE ============
+frappe.ui.form.on("Treatment Mix Component Table", {
+    
+    item(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (row.item) {
+            frappe.call({
+                method: "victoryiron.victoryiron.doctype.induction_furnace_heat.induction_furnace_heat.get_item_valuation_rate",
+                args: { item_code: row.item },
+                callback: function(r) {
+                    if (r.message !== undefined) {
+                        frappe.model.set_value(cdt, cdn, "rate", r.message);
+                        if (row.weight_in_kg) {
+                            let amount = flt(row.weight_in_kg) * flt(r.message);
+                            frappe.model.set_value(cdt, cdn, "amount", amount);
+                        }
+                        calculate_treatment_totals(frm);
+                    }
+                }
+            });
+        }
+    },
+
+    weight_in_kg(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (row.rate) {
+            let amount = flt(row.weight_in_kg) * flt(row.rate);
+            frappe.model.set_value(cdt, cdn, "amount", amount);
+        }
+        calculate_treatment_totals(frm);
+    },
+
+    rate(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (row.weight_in_kg) {
+            let amount = flt(row.weight_in_kg) * flt(row.rate);
+            frappe.model.set_value(cdt, cdn, "amount", amount);
+        }
+        calculate_treatment_totals(frm);
+    },
+
+    table_bych_remove(frm) {
+        calculate_treatment_totals(frm);
+    }
+});
--- victoryiron/victoryiron/doctype/induction_furnace_heat/induction_furnace_heat.py
+++ victoryiron/victoryiron/doctype/induction_furnace_heat/induction_furnace_heat.py
@@ -1,9 +1,140 @@
-# Copyright (c) 2026, beetashokechakraborty and contributors
+# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
+
+# Default items for Charge Mix
+DEFAULT_CHARGE_MIX_ITEMS = [
+    "Sand Pig Iron",
+    "Pig Iron",
+    "M.S.SCRAP - CRC",
+    "MS Scrap-Punching",
+    "MS CI Scrap",
+    "MS Scrap",
+    "CI Foundry Return",
+    "DI Foundry Return",
+    "Carburiser (D.I)",
+    "Ferro Silicon",
+    "Ferro Manganese"
+]
 
 
 class InductionFurnaceHeat(Document):
-	pass
+    def before_insert(self):
+        """Add default items when creating new document"""
+        if not self.charge_mix_component_item:
+            self.add_default_charge_mix_items()
+
+    def add_default_charge_mix_items(self):
+        """Add default charge mix items with their rates"""
+        for item_code in DEFAULT_CHARGE_MIX_ITEMS:
+            # Check if item exists
+            if frappe.db.exists("Item", item_code):
+                rate = get_item_valuation_rate(item_code)
+                self.append("charge_mix_component_item", {
+                    "item": item_code,
+                    "weight_in_kg": 0,
+                    "rate": rate,
+                    "amount": 0
+                })
+
+    def validate(self):
+        self.calculate_charge_mix_totals()
+        self.calculate_treatment_totals()
+        self.calculate_furnace_running_time()
+        self.calculate_furnace_unit_consumed()
+
+    def calculate_charge_mix_totals(self):
+        """Calculate total weight and valuation for charge mix"""
+        total_weight = 0
+        total_valuation = 0
+
+        for row in self.charge_mix_component_item or []:
+            row.amount = flt(row.weight_in_kg) * flt(row.rate)
+            total_weight += flt(row.weight_in_kg)
+            total_valuation += flt(row.amount)
+
+        self.total_charge_mix_kg = flt(total_weight, 2)
+        self.total_charge_mix_valuation = flt(total_valuation, 2)
+
+    def calculate_treatment_totals(self):
+        """Calculate total quantity and valuation for treatment mix"""
+        total_quantity = 0
+        total_valuation = 0
+
+        for row in self.table_bych or []:
+            row.amount = flt(row.weight_in_kg) * flt(row.rate)
+            total_quantity += flt(row.weight_in_kg)
+            total_valuation += flt(row.amount)
+
+        self.total_treatment_quantity = flt(total_quantity, 2)
+        self.total_treatment_valuation = flt(total_valuation, 2)
+
+    def calculate_furnace_running_time(self):
+        """Calculate total furnace running time in hours"""
+        if self.furnace_on_time and self.furnace_off_time:
+            from frappe.utils import time_diff_in_hours
+            hours = time_diff_in_hours(self.furnace_off_time, self.furnace_on_time)
+            # Handle negative (next day case)
+            if hours < 0:
+                hours += 24
+            self.total_furnace_running_time = f"{flt(hours, 2)} Hours"
+
+    def calculate_furnace_unit_consumed(self):
+        """Calculate furnace unit consumed"""
+        if self.furnace_meter_reading_start is not None and self.furnace_meter_reading_end is not None:
+            self.total_furnace_unit_consumed = flt(
+                self.furnace_meter_reading_end - self.furnace_meter_reading_start, 2
+            )
+
+
+@frappe.whitelist()
+def get_item_valuation_rate(item_code):
+    """Get valuation rate from Bin doctype for the given item"""
+    if not item_code:
+        return 0
+
+    # Get valuation rate from Bin
+    bin_rate = frappe.db.get_value(
+        "Bin",
+        {"item_code": item_code},
+        "valuation_rate",
+        order_by="modified desc"
+    )
+
+    if bin_rate:
+        return flt(bin_rate)
+
+    # Fallback: Get from Item master
+    item_rate = frappe.db.get_value("Item", item_code, "valuation_rate")
+    return flt(item_rate) if item_rate else 0
+
+@frappe.whitelist()
+def get_default_charge_mix_items():
+    """Get default charge mix items with their rates in sorted order"""
+    DEFAULT_CHARGE_MIX_ITEMS = [
+        "Sand Pig Iron",
+        "Pig Iron",
+        "M.S.SCRAP - CRC",
+        "MS Scrap-Punching",
+        "MS CI Scrap",
+        "MS Scrap",
+        "CI Foundry Return",
+        "DI Foundry Return",
+        "Carburiser (D.I)",
+        "Ferro Silicon",
+        "Ferro Manganese"
+    ]
+    
+    items = []
+    for item_code in DEFAULT_CHARGE_MIX_ITEMS:
+        if frappe.db.exists("Item", item_code):
+            rate = get_item_valuation_rate(item_code)
+            items.append({
+                "item": item_code,
+                "rate": rate
+            })
+    
+    return items
